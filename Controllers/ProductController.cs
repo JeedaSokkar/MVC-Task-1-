248a734ea56f857ac0047fe70f76bfacf573e3b4
jeeda3 <jeedasokkar2003@gmail.com>
2025-06-25 01:21:35 +0300

    Initial commit - upload MVC project

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public ViewResult Index()
+        {
+            var products = context.Categories.ToList();
+            return View(products);
+        }
+        public ViewResult Create()
+        {
+            return View();
+        }
+        public RedirectToActionResult Add(Category request)
+        {
+            context.Categories.Add(request);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ViewResult Details(int id)
+        {
+            Console.WriteLine(id);
+
+    var product = context.Categories.Find(id);
+
+
+    return View(product);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext .cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(
+                "Server=.;Database=Mvc_1;TrustServerCertificate=True;Trusted_Connection=True");
+        }
+    }
+}
